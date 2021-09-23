df9e6b0ad9b6d14612ff537ccf1d4ee1f5e1ccd5
Антон <sierra_93@mail.ru>
2021-09-24 01:05:53 +0300

    Завел таблицы пользователей и создал схему dbo для обеих БД.

--- dbo/pgsql.garantdb_configuration/Tables/[dbo].[ControlFilters]/[dbo].[ControlFilters]_CreateTable.sql
+++
@@ -1,5 +0,0 @@
--- CREATE TABLE dbo."ControlFilters" (
---     "FilterId" bigserial primary key unique,
---     "FilterKey" varchar(150) not null default '',
---     "FilterValue" varchar(150) not null default ''
--- );
---
+++ dbo/pgsql.garantdb_configuration/Tables/[dbo].[Users]/[dbo].[Users]_AddColumnCode.sql
@@ -0,0 +1,2 @@
+alter table public."AspNetUsers"
+add "Code" character varying not null default '10000';
