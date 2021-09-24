5bc248d26767ed23ff460136cc20c28994f91a3f
Антон <sierra_93@mail.ru>
2021-09-25 01:41:19 +0300

    Добавил столбец записаны ли ответы на вопросы после регистрации.

---
+++ dbo/pgsql.garantdb_test/Tables/[dbo].[Logs]/[dbo].[Logs]_CreateLogsTable.sql
@@ -0,0 +1,8 @@
+create table dbo."Logs" (
+    "LogId" bigserial primary key,
+    "LogLvl" character varying not null default '',
+    "TypeException" character varying not null default '',
+    "Exception" text not null default '',
+    "StackTrace" text not null default '',
+    "Date" timestamp not null default now()
+);
---
+++ dbo/pgsql.garantdb_test/Tables/[dbo].[Users]/[dbo].[Users]_AddColumnIsWriteQuestion.sql
@@ -0,0 +1,2 @@
+alter table public."AspNetUsers"
+add "IsWriteQuestion" bool not null default false;
