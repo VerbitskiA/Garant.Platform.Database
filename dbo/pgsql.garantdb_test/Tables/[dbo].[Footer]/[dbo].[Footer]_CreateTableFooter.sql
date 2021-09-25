8db12b06f9334f056477a823c7634b14cb691c9e
Антон <sierra_93@mail.ru>
2021-09-25 21:53:24 +0300

    Добавил таблицу футера. Добавил данные для тестовой БД.

--- dbo/pgsql.garantdb_test/Tables/[dbo].[Footer]/[dbo].[Footer]_CreateTableFooter.sql
+++ dbo/pgsql.garantdb_test/Tables/[dbo].[Footer]/[dbo].[Footer]_CreateTableFooter.sql
@@ -1,7 +1,9 @@
 create table dbo."Footer" (
-    "FooterId" int primary key,
+    "FooterId" serial primary key,
     "FooterTitle" character varying null,
     "FooterFieldName" character varying null,
     "IsPlace" bool default false,
-    "IsSignleTitle" bool default false
+    "IsSignleTitle" bool default false,
+    "Column" int not null default 0,
+    "Position" int not null default 0
 );
