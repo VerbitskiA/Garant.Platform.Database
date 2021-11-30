305e6242579333b53f72f110e7ebfd88c376dc35
Антон <sierra_93@mail.ru>
2021-11-30 22:36:14 +0300

    Добавил таблицы для заявок франшиз и бизнеса.

--- dbo/pgsql.garantdb_test/Tables/[dbo].[Transitions]/[dbo].[Transitions]_CreateTable.sql
+++ dbo/pgsql.garantdb_test/Tables/[dbo].[Transitions]/[dbo].[Transitions]_CreateTable.sql
@@ -2,5 +2,7 @@ create table if not exists dbo."Transitions" (
     "TransitionId" bigserial primary key,
     "UserId" text not null,
     "TransitionType" varchar(100) not null,
-    "ReferenceId" bigint not null
+    "ReferenceId" bigint not null,
+    "OtherId" text null,
+    "TypeItem" varchar(100) null
 );
