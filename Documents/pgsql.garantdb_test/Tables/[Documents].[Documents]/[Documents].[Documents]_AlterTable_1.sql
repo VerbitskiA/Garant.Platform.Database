8fe60e8b47d73fd5c724d2163f9751f6b0830d2c
Антон <sierra_93@mail.ru>
2021-12-18 02:00:06 +0300

    Изменения в таблице документов.

--- Documents/pgsql.garantdb_test/Tables/[Documents].[Documents]/[Documents].[Documents]_AlterTable_1.sql
+++ Documents/pgsql.garantdb_test/Tables/[Documents].[Documents]/[Documents].[Documents]_AlterTable_1.sql
@@ -1,3 +1,9 @@
 alter table if exists "Documents"."Documents"
 add column if not exists "IsDealDocument" bool not null default false,
-add column if not exists "IsAcceptDocument" bool null default false
+add column if not exists "IsAcceptDocument" bool null,
+add column if not exists "IsSend" bool null,
+add column if not exists "IsRejectDocument" bool null,
+add column if not exists "CommentReject" text null;
+
+alter table if exists "Documents"."Documents"
+rename column "IsAcceptDocument" to "IsApproveDocument";
