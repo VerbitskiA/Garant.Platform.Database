93282cf1187438ff63f877edf95a62ea7275f211
Антон <sierra_93@mail.ru>
2022-01-08 13:25:36 +0300

    Добавил новые поля в таблицу заказов.

--- Commerce/pgsql.garantdb_test/Tables/[Commerce].[Orders]/[Commerce].[Orders]_AlterTable_1.sql
+++ Commerce/pgsql.garantdb_test/Tables/[Commerce].[Orders]/[Commerce].[Orders]_AlterTable_1.sql
@@ -1,2 +1,4 @@
 alter table if exists "Commerce"."Orders"
-add column if not exists "TinkoffSystemOrderId" bigint not null default 0;
+add column if not exists "TinkoffSystemOrderId" bigint not null default 0,
+add column if not exists "Iteration" int not null default 0,
+add column if not exists "OptionalType" varchar(150) null;
