35457198de63745d385d8267194706420bfb9ad7
Антон <sierra_93@mail.ru>
2022-01-09 16:02:59 +0300

    Добавил новые поля.

--- Info/pgsql.garantdb_test/Tables/[Info].[UsersInformation]/[Info].[UsersInformation]_AddColumns_1.sql
+++ Info/pgsql.garantdb_test/Tables/[Info].[UsersInformation]/[Info].[UsersInformation]_AddColumns_1.sql
@@ -1,2 +1,3 @@
 alter table "Info"."UsersInformation"
-add column if not exists "LastName" character varying not null;
+add column if not exists "LastName" character varying not null,
+add column if not exists "DefaultBankName" varchar(400) null;
