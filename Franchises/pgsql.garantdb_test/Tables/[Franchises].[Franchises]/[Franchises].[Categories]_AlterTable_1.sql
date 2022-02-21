157622dfb81de4931461ea2e638954b8b10179f8
Антон <sierra_93@mail.ru>
2022-02-21 23:58:40 +0300

    Добавил новые столбцы в конфиг базу.

--- Franchises/pgsql.garantdb_test/Tables/[Franchises].[Franchises]/[Franchises].[Categories]_AlterTable_1.sql
+++ Franchises/pgsql.garantdb_test/Tables/[Franchises].[Franchises]/[Franchises].[Categories]_AlterTable_1.sql
@@ -1,2 +1,2 @@
-alter table if exists "Franchises"."Categories"
+alter table if exists "Franchises"."FranchiseCategories"
 add column if not exists "FranchiseCategorySysName" varchar(150) not null default ''
