6b81ab684247eef6f4a6b04b5881f74b0f1401db
Антон <sierra_93@mail.ru>
2022-02-22 00:17:39 +0300

    Добавил новые столбцы в конфиг базу.

---
+++ Business/pgsql.garantdb_test/Tables/[Business].[Businesses]/[Business].[Businesses]_AlterTable_1.sql
@@ -0,0 +1,4 @@
+alter table if exists "Business"."Businesses"
+add column if not exists "IsAccepted" bool not null default false,
+add column if not exists "IsRejected" bool not null default false,
+add column if not exists "CommentRejection" text not null default '';
