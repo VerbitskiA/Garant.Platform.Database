4078810a8b12219db76d5c5d017d7e41bc4a8876
Антон <sierra_93@mail.ru>
2022-02-23 00:21:20 +0300

    Изменения в CommentRejection

--- Franchises/pgsql.garantdb_configuration/Tables/[Franchises].[Franchises]/[Franchises].[Franchises]_AlterTable_1.sql
+++ Franchises/pgsql.garantdb_configuration/Tables/[Franchises].[Franchises]/[Franchises].[Franchises]_AlterTable_1.sql
@@ -1,4 +1,4 @@
 alter table if exists "Franchises"."Franchises"
 add column if not exists "IsAccepted" bool not null default false,
 add column if not exists "IsRejected" bool not null default false,
-add column if not exists "CommentRejection" text not null default '';
+add column if not exists "CommentRejection" text null default '';
