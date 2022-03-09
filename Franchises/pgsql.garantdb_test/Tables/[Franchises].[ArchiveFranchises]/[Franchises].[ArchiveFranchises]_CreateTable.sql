a78cfad8526ca15ab9d03f4c0c890c163f16a20f
VerbitskiA <eternity.2019@mail.ru>
2022-03-09 13:38:59 +0300

    Статус об архивации хранится теперь в таблице Франшиз.

--- Franchises/pgsql.garantdb_test/Tables/[Franchises].[ArchiveFranchises]/[Franchises].[ArchiveFranchises]_CreateTable.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists "Franchises"."ArchiveFranchises" (
-    "ArchiveFranchiseId" bigserial primary key,
-    "FranchiseId" bigint references "Franchises"."Franchises"("FranchiseId"),
-    "IsArchive" boolean DEFAULT false,
-    "ArchivingDate" timestamp
-);
